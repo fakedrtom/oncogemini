5cbff4dd600b6822257bf5d46c1d66f463a5be0c
Thomas Nicholas <tom@d151-91.genetics.utah.edu>
2018-05-15 15:58:09 -0600

    added --samples, --minDP, --minGQ functionality to the parameters from bottleneck script, updated output to incude AFs

--- gemini/gemini_truncal.py
+++ gemini/gemini_truncal.py
@@ -56,30 +56,57 @@ def truncal(parser, args):
     # and only 1 patient is present in the database
     # that patient will be used
     # also verify that patient is among possible patient_ids
+    # sample names are saved to patient specific dict
     patients = []
+    names = {}
     for row in gq:
         patients.append(row['patient_id'])
+        if row['patient_id'] not in names:
+            names[row['patient_id']] = []
+        names[row['patient_id']].append(row['name'])
     if args.patient is None and len(set(patients)) == 1:
         patient = patients[0]
     elif args.patient is None and len(set(patients)) > 1:
         raise NameError('More than 1 patient is present, specify a patient_id with --patient')
     if patient not in patients:
         raise NameError('Specified patient is not found, check the ped file for available patient_ids')
+
+    # check that specified samples with --samples are present
+    # otherwise all names for given patient from ped will asigned to samples list
+    if samples != 'All':
+        for sample in samples:
+            if sample not in names[patient]:
+                raise NameError('Specified samples, ' + sample + ', is not found')
+    elif samples == 'All':
+        samples = names[patient]
     
     # iterate again through each sample and save which sample is the normal
-    # non-normal sample names are saved to a list
+    # non-normal, tumor sample names are saved to a list
+    # establish which timepoints belong to which samples names
+    # this is done for the specified --patient and --samples
+    # designate the last and first time points
     gq.run(query)
     normal_samples = []
     tumor_samples = []
+    timepoints = {}
     for row in gq:
-        if int(row['time']) == 0 and row['patient_id'] == patient:
-            normal_samples.append(row['name'])
-        elif int(row['time']) > 0 and row['patient_id'] == patient:
-            tumor_samples.append(row['name'])
-
+        if row['patient_id'] == patient and row['name'] in samples:
+            if int(row['time']) == 0:
+                normal_samples.append(row['name'])
+            elif int(row['time']) > 0:
+                tumor_samples.append(row['name'])
+            if int(row['time']) not in timepoints:
+                timepoints[int(row['time'])] = []
+            timepoints[int(row['time'])].append(row['name'])
+#    endpoint = max(timepoints.keys())
+#    startpoint = min(timepoints.keys())
+#    times = sorted(timepoints.keys(), reverse=True)
+    
     # check arrays to see if samples have been added
     # if arrays are empty there is probably a problem in samples
     # check the ped file being loaded into the db
+    if len(normal_samples) == 0 and len(tumor_samples) == 0:
+        raise NameError('There are no samples; check the ped file for proper format and loading')
     if len(normal_samples) == 0:
         raise NameError('There are no normal samples; check the ped file for proper format and loading')
     if len(tumor_samples) == 0:
@@ -121,6 +148,36 @@ def truncal(parser, args):
     # execute the truncal query (but don't do anything with the results)"
     gq.run(query, gt_filter)
 
+    # get the sample index numbers so we can get sample specific GT info (AFs, DPs, etc.)
+    smp2idx = gq.sample_to_idx
+
+    # print header and add the AFs of included samples and the calculated slope
+    addHeader = []
+    for key in timepoints:
+        for s in timepoints[key]:
+            if s in samples:
+                af = 'alt_AF.' + s
+                addHeader.append(af)
+    print(gq.header) + "\t" + '\t'.join(addHeader)
+
     # iterate through each row of the truncal results and print
     for row in gq:
-        print(row)
+        depths = []
+        quals = []
+        addEnd = []
+        for key in timepoints:
+            for s in timepoints[key]:
+                if s in samples:
+                    smpidx = smp2idx[s]
+                    sampleDP = row['gt_depths'][smpidx]
+                    depths.append(sampleDP)
+                    sampleGQ = row['gt_quals'][smpidx]
+                    quals.append(sampleGQ)
+                    sampleAF = row['gt_alt_freqs'][smpidx]
+                    addEnd.append(str(sampleAF))
+        
+        if min(depths) < minDP or min(quals) < minGQ:
+            continue
+        # print results that meet the requirements
+        # add selected sample AFs
+        print str(row) + "\t" + '\t'.join(addEnd)
