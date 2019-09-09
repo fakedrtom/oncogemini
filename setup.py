1951cdff87ba39c299dad0d2ce605ac123808c26
fakedrtom <thomasjnicholas@gmail.com>
2019-09-09 17:09:49 -0600

    updating setup.py to oncogemini to have it stop rewriting gemini

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
 import os
 from setuptools import setup
 
-version_py = os.path.join(os.path.dirname(__file__), 'gemini', 'version.py')
+version_py = os.path.join(os.path.dirname(__file__), 'oncogemini', 'version.py')
 version = open(version_py).read().strip().split('=')[-1].replace('"', '').strip()
 long_description = """
-``gemini`` is a database framework for exploring genetic variation'
+``oncogemini`` is a database framework for exploring genetic variation in tumor samples'
 """
 
 with open("requirements.txt", "r") as f:
     install_requires = [x.strip() for x in f.readlines() if not
-                        x.startswith(("gemini", "http", "git"))]
+                        x.startswith(("oncogemini", "http", "git"))]
 
 setup(
-        name="gemini",
+        name="oncogemini",
         version=version,
         install_requires=install_requires,
         requires=['python (>=2.5, <3.0)'],
-        packages=['gemini',
-                  'gemini.scripts',
-                  'gemini.tests',
-                  'gemini.data'],
-        author="Aaron Quinlan and Uma Paila",
-        description='A database framework for exploring genetic variation',
+        packages=['oncogemini',
+                  'oncogemini.scripts',
+                  'oncogemini.tests',
+                  'oncogemini.data'],
+        author="Thomas Nicholas",
+        description='A database framework for exploring genetic variation in tumor samples',
         long_description=long_description,
-        url="http://gemini.readthedocs.org",
-        package_dir={'gemini': "gemini"},
-        package_data={'gemini': [
+        url="http://oncogemini.readthedocs.org",
+        package_dir={'oncogemini': "oncogemini"},
+        package_data={'oncogemini': [
             'static/css/gemini.css',
             'static/img/gemini.png',
             'static/third_party/bootstrap/css/*',
@@ -37,8 +37,8 @@ setup(
             ]},
         zip_safe=False,
         include_package_data=True,
-        scripts=['gemini/scripts/oncogemini'],
-        author_email="arq5x@virginia.edu",
+        scripts=['oncogemini/scripts/oncogemini'],
+        author_email="thomas.nicholas@utah.edu",
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Science/Research',
