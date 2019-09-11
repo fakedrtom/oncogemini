5b45bca74dd1c2264af612cf2d9b2c11f69606c8
fakedrtom <thomasjnicholas@gmail.com>
2019-09-11 16:31:08 -0600

    changed cancer_gemini to oncogemini

--- test/test-common-parameters.sh
+++ test/test-common-parameters.sh
@@ -17,7 +17,7 @@ printf "    bottleneck.patientB.minDP10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287	0.0554716911435	-0.025333518655	0.73779842863
 9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0178571428571	0.35	0.264150943396	0.0878301886792	-0.0492587601078	0.827307416385" > exp
-cancer_gemini bottleneck \
+oncogemini bottleneck \
     --patient B \
     --minDP 10 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -28,7 +28,7 @@ rm obs exp
 printf "    bottleneck.patientB.minDP100...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287	0.0554716911435	-0.025333518655	0.73779842863" > exp
-cancer_gemini bottleneck \
+oncogemini bottleneck \
     --patient B \
     --minDP 100 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -39,7 +39,7 @@ rm obs exp
 printf "    loh.patientB.minDP10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4
 13	32912963	32912968	TGAAA	T	BRCA2	0.507246376812	0.986666666667	0.975	0.890625	0.875" > exp
-cancer_gemini loh \
+oncogemini loh \
     --patient B \
     --minDP 10 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -49,7 +49,7 @@ rm obs exp
 
 printf "    loh.patientB.minDP49...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4" > exp
-cancer_gemini loh \
+oncogemini loh \
     --patient B \
     --minDP 49 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -61,7 +61,7 @@ printf "    truncal.patientC.minDP10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2
 5	56168738	56168740	AC	A	MAP3K1	0.0	0.492753623188	0.476923076923
 5	56183305	56183306	C	T	MAP3K1	0.0	0.415730337079	0.341772151899" > exp
-cancer_gemini truncal \
+oncogemini truncal \
     --patient C \
     --minDP 10 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -72,7 +72,7 @@ rm obs exp
 printf "    truncal.patientC.minDP79...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2
 5	56183305	56183306	C	T	MAP3K1	0.0	0.415730337079	0.341772151899" > exp
-cancer_gemini truncal \
+oncogemini truncal \
     --patient C \
     --minDP 79 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -84,7 +84,7 @@ printf "    unique.patientC.minDP10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 6	152419921	152419922	T	A	ESR1	0.404255319149
 16	68842399	68842400	G	C	CDH1	0.588235294118" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --minDP 10 \
@@ -96,7 +96,7 @@ rm obs exp
 printf "    unique.patientC.minDP35...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 6	152419921	152419922	T	A	ESR1	0.404255319149" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --minDP 35 \
@@ -114,7 +114,7 @@ printf "    bottleneck.patientB.minGQ10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287	0.0554716911435	-0.025333518655	0.73779842863
 9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0178571428571	0.35	0.264150943396	0.0878301886792	-0.0492587601078	0.827307416385" > exp
-cancer_gemini bottleneck \
+oncogemini bottleneck \
     --patient B \
     --minGQ 10 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -125,7 +125,7 @@ rm obs exp
 printf "    bottleneck.patientB.minGQ141...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287	0.0554716911435	-0.025333518655	0.73779842863" > exp
-cancer_gemini bottleneck \
+oncogemini bottleneck \
     --patient B \
     --minGQ 141 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -136,7 +136,7 @@ rm obs exp
 printf "    loh.patientB.minGQ10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4
 13	32912963	32912968	TGAAA	T	BRCA2	0.507246376812	0.986666666667	0.975	0.890625	0.875" > exp
-cancer_gemini loh \
+oncogemini loh \
     --patient B \
     --minGQ 10 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -146,7 +146,7 @@ rm obs exp
 
 printf "    loh.patientB.minGQ20...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4" > exp
-cancer_gemini loh \
+oncogemini loh \
     --patient B \
     --minGQ 20 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -158,7 +158,7 @@ printf "    truncal.patientB.minGQ10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287
 6	152419919	152419920	T	C	ESR1	0.0	0.552631578947	0.463414634146	0.383333333333	0.40625" > exp
-cancer_gemini truncal \
+oncogemini truncal \
     --patient B \
     --minGQ 10 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -169,7 +169,7 @@ rm obs exp
 printf "    truncal.patientB.minGQ160...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4
 6	152419919	152419920	T	C	ESR1	0.0	0.552631578947	0.463414634146	0.383333333333	0.40625" > exp
-cancer_gemini truncal \
+oncogemini truncal \
     --patient B \
     --minGQ 160 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -181,7 +181,7 @@ printf "    unique.patientC.minGQ10...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 6	152419921	152419922	T	A	ESR1	0.404255319149
 16	68842399	68842400	G	C	CDH1	0.588235294118" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --minGQ 10 \
@@ -193,7 +193,7 @@ rm obs exp
 printf "    unique.patientC.minGQ144...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 6	152419921	152419922	T	A	ESR1	0.404255319149" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --minGQ 144 \
@@ -212,7 +212,7 @@ echo "chrom	start	end	ref	alt	gene	alt_AF.B1	alt_AF.B2	alt_AF.B3	slope	intercept
 3	10089729	10089730	C	T	FANCD2	0.00588235294118	0.00806451612903	0.267605633803	0.130861640431	-0.0370108061398	0.869627975964
 6	152419921	152419922	T	A	ESR1	0.0	1.0	0.571428571429	0.285714285714	0.238095238095	0.569494797451
 9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0178571428571	0.35	0.175	-0.052380952381	0.88778411242" > exp
-cancer_gemini bottleneck \
+oncogemini bottleneck \
     --patient B \
     --samples B1,B2,B3 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -223,7 +223,7 @@ rm obs exp
 printf "    loh.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B2	alt_AF.B3
 13	32912963	32912968	TGAAA	T	BRCA2	0.507246376812	0.975	0.890625" > exp
-cancer_gemini loh \
+oncogemini loh \
     --patient B \
     --samples B0,B2,B3 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -235,7 +235,7 @@ printf "    truncal.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B4
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.146496815287
 6	152419919	152419920	T	C	ESR1	0.0	0.552631578947	0.40625" > exp
-cancer_gemini truncal \
+oncogemini truncal \
     --patient B \
     --samples B0,B1,B4 \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -247,7 +247,7 @@ printf "    unique.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C1
 5	56168738	56168740	AC	A	MAP3K1	0.492753623188
 5	56183305	56183306	C	T	MAP3K1	0.415730337079" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --samples C0,C1 \
     --specific C1 \
@@ -265,7 +265,7 @@ printf "    bottleneck.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	raw.alt_AF.B0	alt_AF.B1	raw.alt_AF.B1	alt_AF.B2	raw.alt_AF.B2	alt_AF.B3	raw.alt_AF.B3	alt_AF.B4	raw.alt_AF.B4	slope	intercept	r_value
 3	10089729	10089730	C	T	FANCD2	0.0	0.0	0.0588235294118	0.00588235294118	0.0806451612903	0.00806451612903	0.892018779343	0.267605633803	0.292993630573	0.146496815287	0.141918251108	-0.0189402820919	0.610347968039
 9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0	0.0	0.178571428571	0.0178571428571	1	0.35	0.528301886792	0.264150943396	0.205660377358	-0.0699460916442	0.762067229018" > exp
-cancer_gemini bottleneck \
+oncogemini bottleneck \
     --patient B \
     --purity \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -276,7 +276,7 @@ rm obs exp
 printf "    loh.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	raw.alt_AF.B0	alt_AF.B1	raw.alt_AF.B1	alt_AF.B2	raw.alt_AF.B2	alt_AF.B3	raw.alt_AF.B3	alt_AF.B4	raw.alt_AF.B4
 13	32912963	32912968	TGAAA	T	BRCA2	0.507246376812	0.507246376812	1	0.986666666667	1	0.975	1	0.890625	1	0.875" > exp
-cancer_gemini loh \
+oncogemini loh \
     --patient B \
     --purity \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -288,7 +288,7 @@ printf "    truncal.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	raw.alt_AF.C0	alt_AF.C1	raw.alt_AF.C1	alt_AF.C2	raw.alt_AF.C2
 5	56168738	56168740	AC	A	MAP3K1	0.0	0.0	1	0.492753623188	0.596153846154	0.476923076923
 5	56183305	56183306	C	T	MAP3K1	0.0	0.0	1	0.415730337079	0.427215189873	0.341772151899" > exp
-cancer_gemini truncal \
+oncogemini truncal \
     --patient C \
     --purity \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -300,7 +300,7 @@ printf "    unique.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2	raw.alt_AF.C2
 6	152419921	152419922	T	A	ESR1	0.505319148936	0.404255319149
 16	68842399	68842400	G	C	CDH1	0.735294117647	0.588235294118" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --purity \
@@ -317,7 +317,7 @@ printf "testing --cancers parameter...\n"
 printf "    bottleneck.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287	0.0554716911435	-0.025333518655	0.73779842863" > exp
-cancer_gemini bottleneck \
+oncogemini bottleneck \
     --patient B \
     --cancers AML,ST \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -327,7 +327,7 @@ rm obs exp
 
 printf "    loh.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4" > exp
-cancer_gemini loh \
+oncogemini loh \
     --patient B \
     --cancers AML,ST \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -338,7 +338,7 @@ rm obs exp
 printf "    truncal.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4
 3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287" > exp
-cancer_gemini truncal \
+oncogemini truncal \
     --patient B \
     --cancers AML,ST \
     --columns "chrom,start,end,ref,alt,gene" \
@@ -349,7 +349,7 @@ rm obs exp
 printf "     unique.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 16	68842399	68842400	G	C	CDH1	0.588235294118" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --cancers AML,ST \
