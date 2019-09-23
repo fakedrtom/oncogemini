fbfbe8d1580369d6fa8885061e387c09cc46be07
fakedrtom <thomasjnicholas@gmail.com>
2019-09-23 15:33:09 -0600

    changed gemini to onocgemini, replaced tests using test5.snpeff.db with test3.snpeff.db

--- test/test-stats.sh
+++ test/test-stats.sh
@@ -1,13 +1,23 @@
+check() 
+{
+    if diff <( sort "$1" ) <( sort "$2" ); then
+        echo ok
+    else
+        echo fail
+        exit 1
+    fi
+}
+
 ###################################################################
 # 1. Test variants by sample
 ###################################################################
 echo "    stat.t01...\c"
 echo "sample	total
-M10475	4
-M10478	6
-M10500	6
-M128215	6" > exp
-gemini stats --vars-by-sample test5.snpeff.db \
+1094PC0005	1
+1094PC0009	1
+1094PC0012	5
+1094PC0013	4" > exp
+oncogemini stats --vars-by-sample test3.snpeff.db \
        > obs
 check obs exp
 rm obs exp
@@ -17,11 +27,11 @@ rm obs exp
 ###################################################################
 echo "    stat.t02...\c"
 echo "sample	num_hom_ref	num_het	num_hom_alt	num_unknown	total
-M10475	4	1	3	1	9
-M10478	2	2	4	1	9
-M10500	2	1	5	1	9
-M128215	2	3	3	1	9" > exp
-gemini stats --gts-by-sample test5.snpeff.db \
+1094PC0005	4	0	1	3	8
+1094PC0009	4	0	1	3	8
+1094PC0012	3	2	3	0	8
+1094PC0013	3	2	2	1	8" > exp
+oncogemini stats --gts-by-sample test3.snpeff.db \
        > obs
 check obs exp
 rm obs exp
@@ -31,11 +41,11 @@ rm obs exp
 ###################################################################
 echo "    stat.t03...\c"
 echo "aaf	count
-0.125	2
-0.375	1
-0.5	2
-1.0	4" > exp
-gemini stats --sfs test5.snpeff.db \
+0.0	3
+0.25	1
+0.5	1
+1.0	3" > exp
+oncogemini stats --sfs test3.snpeff.db \
        > obs
 check obs exp
 rm obs exp
@@ -50,7 +60,7 @@ C->T	1
 G->A	1
 G->C	1
 G->T	1" > exp
-gemini stats --snp-counts test3.snpeff.db \
+oncogemini stats --snp-counts test3.snpeff.db \
         > obs
 
 check obs exp
@@ -62,7 +72,7 @@ rm obs exp
 echo "    stat.t05...\c"
 echo "ts	tv	ts/tv
 4	5	0.8" > exp
-gemini stats --tstv test.snpeff.vcf.db > obs
+oncogemini stats --tstv test.snpeff.vcf.db > obs
 check obs exp
 rm obs exp
 
@@ -72,7 +82,7 @@ rm obs exp
 echo "    stat.t06...\c"
 echo "ts	tv	ts/tv
 3	2	1.5" > exp
-gemini stats --tstv-coding test.snpeff.vcf.db > obs
+oncogemini stats --tstv-coding test.snpeff.vcf.db > obs
 check obs exp
 rm obs exp
 
@@ -82,7 +92,7 @@ rm obs exp
 echo "    stat.t07...\c"
 echo "ts	tv	ts/tv
 1	3	0.3333" > exp
-gemini stats --tstv-noncoding test.snpeff.vcf.db > obs
+oncogemini stats --tstv-noncoding test.snpeff.vcf.db > obs
 check obs exp
 rm obs exp
 
@@ -92,23 +102,23 @@ rm obs exp
 ###################################################################
 echo "    stat.t08...\c"
 echo "sample1	sample2	distance
-M10500	M10500	0.0
-M10475	M10475	0.0
-M128215	M10500	2.5
-M10478	M10478	0.0
-M10475	M10500	2.0
-M10500	M128215	2.5
-M10475	M10478	1.25
-M10500	M10475	2.0
-M128215	M10478	1.7143
-M10478	M10500	0.5714
-M10475	M128215	0.5714
-M10478	M128215	1.7143
-M128215	M10475	0.5714
-M128215	M128215	0.0
-M10478	M10475	1.25
-M10500	M10478	0.5714" > exp
-gemini stats --mds test5.snpeff.db \
+1094PC0013	1094PC0013	0.0
+1094PC0013	1094PC0012	0.0
+1094PC0009	1094PC0009	0.0
+1094PC0005	1094PC0013	0.25
+1094PC0013	1094PC0005	0.25
+1094PC0005	1094PC0012	0.25
+1094PC0012	1094PC0013	0.0
+1094PC0013	1094PC0009	0.4
+1094PC0012	1094PC0012	0.0
+1094PC0009	1094PC0005	0.25
+1094PC0009	1094PC0013	0.4
+1094PC0005	1094PC0005	0.0
+1094PC0012	1094PC0009	0.4
+1094PC0009	1094PC0012	0.4
+1094PC0005	1094PC0009	0.25
+1094PC0012	1094PC0005	0.25" > exp
+oncogemini stats --mds test.snpeff.vcf.db \
       > obs
 check obs exp
 rm obs exp
@@ -118,10 +128,10 @@ rm obs exp
 ###################################################################
 echo "    stat.t09...\c"
 echo "sample	total	num_het	num_hom_alt	num_hom_ref
-M10475	4	1	3	4
-M128215	6	3	3	2
-M10478	6	2	4	2
-M10500	6	1	5	2" > exp
-gemini stats --summarize "select * from variants" test5.snpeff.db > obs
+1094PC0005	1	0	1	4
+1094PC0009	1	0	1	4
+1094PC0012	5	2	3	3
+1094PC0013	4	2	2	3" > exp
+oncogemini stats --summarize "select * from variants" test3.snpeff.db > obs
 check obs exp
 rm obs exp
