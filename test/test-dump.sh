cc771b4b6c351b7ced71c027fecbfbc48a3b206a
fakedrtom <thomasjnicholas@gmail.com>
2019-09-23 16:45:51 -0600

    added check, changed gemini to oncogemini, updated exp

--- test/test-dump.sh
+++ test/test-dump.sh
@@ -1,11 +1,21 @@
+check() 
+{
+    if diff <( sort "$1" ) <( sort "$2" ); then
+        echo ok
+    else
+        echo fail
+	exit 1
+    fi
+}
+
 ####################################################################
 # 1. Test the TFAM dump
 ####################################################################
 echo "    dump.t01...\c"
-echo "1 M10475 0 0 1 1
+echo "1 M10475 None None 1 1
 1 M10478 M10475 M10500 2 2
-1 M10500 0 0 2 2
+1 M10500 None None 2 2
 1 M128215 M10475 M10500 1 1" > exp
-gemini dump --tfam test4.snpeff.ped.db > obs
+oncogemini dump --tfam test4.snpeff.ped.db > obs
 check obs exp
 rm obs exp
