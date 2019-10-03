3d4a3e409c52bcb3a71e377c28f8b2e8f3ec6412
fakedrtom <thomasjnicholas@gmail.com>
2019-10-03 12:46:33 -0600

    updated check function

--- test/test-stats.sh
+++ test/test-stats.sh
@@ -1,12 +1,12 @@
-check() 
+check()
 {
-    if diff <( sort "$1" ) <( sort "$2" ); then
-        echo ok
-    else
-        echo fail
-        exit 1
-    fi
+	if diff $1 $2; then
+    	echo ok
+	else
+    	echo fail
+	fi
 }
+export -f check
 
 ###################################################################
 # 1. Test variants by sample
