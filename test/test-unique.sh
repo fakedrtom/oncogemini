6489c7477ff82c85377de23c937712e2fda12177
fakedrtom <thomasjnicholas@gmail.com>
2019-10-03 12:46:50 -0600

    updated check function

--- test/test-unique.sh
+++ test/test-unique.sh
@@ -1,13 +1,12 @@
-check() 
+check()
 {
-    if diff <( sort "$1" ) <( sort "$2" ); then
-        echo ok
-    else
-        echo fail
-        exit 1
-    fi
+	if diff $1 $2; then
+    	echo ok
+	else
+    	echo fail
+	fi
 }
-#export -f check
+export -f check
 
 ###################################################################
 # 1. Test basic functionality
@@ -15,21 +14,21 @@ check()
 printf "testing basic functionality...\n"
 printf "    unique.patientA...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.A3" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient A \
     --specific A3 \
     --columns "chrom,start,end,ref,alt,gene" \
-    functional_tests_tools.db > obs
+    oncogemini_test.db > obs
 check obs exp
 rm obs exp
 
 printf "    unique.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B1" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient B \
     --specific B1 \
     --columns "chrom,start,end,ref,alt,gene" \
-    functional_tests_tools.db > obs
+    oncogemini_test.db > obs
 check obs exp
 rm obs exp
 
@@ -37,11 +36,11 @@ printf "    unique.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 6	152419921	152419922	T	A	ESR1	0.404255319149
 16	68842399	68842400	G	C	CDH1	0.588235294118" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --columns "chrom,start,end,ref,alt,gene" \
-    functional_tests_tools.db > obs
+    oncogemini_test.db > obs
 check obs exp
 rm obs exp
 
@@ -52,36 +51,36 @@ printf "testing --maxOthers parameter...\n"
 printf "    unique.patientA...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.A1
 16	68842399	68842400	G	C	CDH1	0.606060606061" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient A \
     --specific A1 \
     --maxOthers 0.5 \
     --columns "chrom,start,end,ref,alt,gene" \
-    functional_tests_tools.db > obs
+    oncogemini_test.db > obs
 check obs exp
 rm obs exp
 
 printf "    unique.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B1
 6	152419919	152419920	T	C	ESR1	0.552631578947" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient B \
     --specific B1 \
     --maxOthers 0.5 \
     --columns "chrom,start,end,ref,alt,gene" \
-    functional_tests_tools.db > obs
+    oncogemini_test.db > obs
 check obs exp
 rm obs exp
 
 printf "    unique.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 16	68842399	68842400	G	C	CDH1	0.588235294118" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --maxOthers 0.5 \
     --columns "chrom,start,end,ref,alt,gene" \
-    functional_tests_tools.db > obs
+    oncogemini_test.db > obs
 check obs exp
 rm obs exp
 
@@ -92,11 +91,11 @@ printf "testing --increase parameter...\n"
 printf "    unique.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C2
 16	68842399	68842400	G	C	CDH1	0.588235294118" > exp
-cancer_gemini unique \
+oncogemini unique \
     --patient C \
     --specific C2 \
     --increase 0.5 \
     --columns "chrom,start,end,ref,alt,gene" \
-    functional_tests_tools.db > obs
+    oncogemini_test.db > obs
 check obs exp
 rm obs exp
