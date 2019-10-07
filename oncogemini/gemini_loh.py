dc3be334183e39079958aaef66f19c5a695f2c35
fakedrtom <thomasjnicholas@gmail.com>
2019-10-07 16:16:55 -0600

    removed old commented out lines

--- oncogemini/gemini_loh.py
+++ oncogemini/gemini_loh.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 from __future__ import absolute_import
-
 from . import GeminiQuery
 from . import gemini_utils as utils
 import sys
-#from . import sql_utils
 
 # LOH mutations are categorized as being heterozygous 
 # in a normal tissue sample, but have risen homozygous 
@@ -48,15 +46,6 @@ def loh(parser, args):
         query = "pragma table_info(variants)"
         gq.run(query)
         utils.check_cancer_annotations(gq)
-#        cancer_abbrevs = 0
-#        for row in gq:
-#            fields = str(row).rstrip('\n').split('\t')
-#            if fields[1] == 'civic_gene_abbreviations':
-#                cancer_abbrevs += 1
-#            if fields[1] == 'cgi_gene_abbreviations':
-#                cancer_abbrevs += 1
-#        if cancer_abbrevs == 0:
-#            raise NameError('No civic_gene_abbreviations or cgi_gene_abbreviations found in database, cannot use --cancers')
         cancers = args.cancers.split(',')
     if args.maxNorm is None:
         maxNorm = float(0.7)
@@ -75,7 +64,6 @@ def loh(parser, args):
         purity = {}
         gq.run(query)
         utils.get_purity(gq, purity)
-#    else:
 
     # define sample search query    
     query = "select patient_id, name, time from samples"
@@ -91,32 +79,9 @@ def loh(parser, args):
     # sample names are saved to patient specific dict
     patients = []
     names = {}
-#    purity = {}
     utils.get_names(gq,patients,names)
     patient = utils.get_patient(patient,patients)
     samples = utils.get_samples(patient,names,samples)
-#    for row in gq:
-#        patients.append(row['patient_id'])
-#        if row['patient_id'] not in names:
-#            names[row['patient_id']] = []
-#        names[row['patient_id']].append(row['name'])
-#        if args.purity:
-#            purity[row['name']] = float(row['purity'])
-#    if args.patient is None and len(set(patients)) == 1:
-#        patient = patients[0]
-#    elif args.patient is None and len(set(patients)) > 1:
-#        raise NameError('More than 1 patient is present, specify a patient_id with --patient')
-#    if patient not in patients:
-#        raise NameError('Specified patient is not found, check the ped file for available patient_ids')
-
-    # check that specified samples with --samples and/or --specific are present
-    # otherwise all names for given patient from ped will asigned to samples list
-#    if samples != 'All':
-#        for sample in samples:
-#            if sample not in names[patient]:
-#                raise NameError('Specified samples, ' + sample + ', is not found')
-#    elif samples == 'All':
-#        samples = names[patient]
     if somatic != 'none' and somatic not in samples:
         sys.exit("Error: Specified sample name with --specific is not found, make sure a single sample only is provided and check the sample manifest file for available sample names")
 
@@ -131,17 +96,6 @@ def loh(parser, args):
     timepoints = {}
     samples_tps = {}
     utils.sort_samples(gq,normal_samples,tumor_samples,timepoints,samples_tps,patient,samples)
-#    for row in gq:
-#        if row['patient_id'] == patient and row['name'] in samples:
-#            if int(row['time']) == 0:
-#                normal_samples.append(row['name'])
-#            elif int(row['time']) > 0:
-#                tumor_samples.append(row['name'])
-#            if int(row['time']) not in timepoints:
-#                timepoints[int(row['time'])] = []
-#            timepoints[int(row['time'])].append(row['name'])
-#            samples_tps[row['name']] = int(row['time'])
-#    endpoint = max(timepoints.keys())
     startpoint = min(timepoints.keys())
 
     # if only sample included with --specific is the first timepoint, --specific won't work
@@ -149,13 +103,9 @@ def loh(parser, args):
         if samples_tps[somatic] == startpoint:
             sys.exit("Error: Specified sample with --specific is the first timepoint, specify a sample that has a preceding sample")
 
-#    times = sorted(timepoints.keys(), reverse=True)
-    
     # check arrays to see if samples have been added
     # if arrays are empty there is probably a problem in samples
     # check the ped file being loaded into the db
-#    if len(normal_samples) == 0 and len(tumor_samples) == 0:
-#        raise NameError('There are no samples; check the ped file for proper format and loading')
     if len(normal_samples) == 0 and somatic == 'none':
         sys.exit("Error: There are no normal samples; check the sample manifest file for proper format and loading")
     if len(tumor_samples) == 0 and somatic == 'none':
@@ -177,32 +127,8 @@ def loh(parser, args):
     else:
         filter = str(1)
     query = utils.make_query(columns,filter)
-#    if args.columns is not None:
-        # the user only wants to report a subset of the columns
-#        if cancers == 'none':
-#            query = "SELECT " + args.columns + " FROM variants"
-#        elif cancers != 'none':
-#            query = "SELECT " + args.columns + ",civic_gene_abbreviations,cgi_gene_abbreviations FROM variants"
-#    else:
-        # report the kitchen sink
-#        query = "SELECT * FROM variants"
-#    if args.filter is not None:
-        # add any non-genotype column limits to the where clause
-#        query += " WHERE " + args.filter
-    # query = "select chrom, start, end, gt_alt_freqs, gt_types from variants where impact_severity !='LOW' and (max_evi =='A' or max_evi == 'B' or max_rating >= 4)"
 
-    # create gt_filter command using saved sample info
-#    filter_cmd = ""
-#    for sample in normal_samples:
-#        filter_cmd += "(gt_alt_freqs." + sample + " >= " + minNorm + " and gt_alt_freqs." + sample + " <= " + maxNorm + ") and "
-#    for sample in tumor_samples:
-#        if sample == tumor_samples[len(tumor_samples)-1]:
-#            filter_cmd += "gt_alt_freqs." + sample + " > " + minTumor
-#            continue 
-#        filter_cmd += "gt_alt_freqs." + sample + " > " + minTumor + " and " 
-#    gt_filter = filter_cmd
-
-    # execute the truncal query (but don't do anything with the results)"
+    # execute a new query to process the variants
     gq.run(query)#, gt_filter)
 
     # get the sample index numbers so we can get sample specific GT info (AFs, DPs, etc.)
@@ -238,7 +164,6 @@ def loh(parser, args):
         if args.purity:
             raw = 'raw.alt_AF.' + somatic
             addHeader.append(raw)
-#    print(gq.header) + "\t" + '\t'.join(addHeader)
     print '\t'.join(addHeader)
 
     # iterate through each row of the results and print
@@ -264,8 +189,6 @@ def loh(parser, args):
                             rawAF = row['gt_alt_freqs'][smpidx]
                         else:
                             sampleAF = row['gt_alt_freqs'][smpidx]
-#                        if sampleAF > 1:
-#                            sampleAF = 1
                         if s in normal_samples and sampleAF >= 0:
                             normAFs.append(sampleAF)
                         if s in tumor_samples and sampleAF >= 0:
@@ -286,8 +209,6 @@ def loh(parser, args):
                     rawAF = row['gt_alt_freqs'][smpidx]
                 else:
                     sampleAF = row['gt_alt_freqs'][smpidx]
-#                if sampleAF > 1:
-#                    sampleAF = 1
                 if sampleAF >= 0:
                     normAFs.append(sampleAF)
                 sampleDP = row['gt_depths'][smpidx]
@@ -303,8 +224,6 @@ def loh(parser, args):
                 rawAF = row['gt_alt_freqs'][smpidx]
             else:
                 sampleAF = row['gt_alt_freqs'][smpidx]
-#            if sampleAF > 1:
-#                sampleAF = 1
             if sampleAF >= 0:
                 tumsAFs.append(sampleAF)
             sampleDP = row['gt_depths'][smpidx]
@@ -337,6 +256,5 @@ def loh(parser, args):
                     include += 1
             if include > 0:
                 print '\t'.join(output)
-#                print str(row) + "\t" + '\t'.join(addEnd)
         else:
             print '\t'.join(output)
