edd9061b601312881a3b458e23303a264a89a734
Thomas Nicholas <u0055382@kingspeak23.wasatch.peaks>
2019-10-24 12:57:20 -0600

    adjusted significant digits in test

--- test/test-loh.sh
+++ test/test-loh.sh
@@ -12,24 +12,18 @@ export -f check
 # 1. Test basic functionality
 ###################################################################
 printf "testing basic functionality...\n"
-
-#printf "    loh.patientA...\n"
-#echo "NameError: There are no normal samples; check the ped file for proper format and loading" > exp
-
-#oncogemini loh \
-#    --patient A \
-#    --columns "chrom,start,end,ref,alt,gene" \
-#    oncogemini_test.db | tail -1 > obs
-#check obs exp
-#rm obs exp
-
 printf "    loh.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4
-13	32912963	32912968	TGAAA	T	BRCA2	0.507246376812	0.986666666667	0.975	0.890625	0.875" > exp
+13	32912963	32912968	TGAAA	T	BRCA2	0.507	0.987	0.975	0.891	0.875" > exp
 oncogemini loh \
     --patient B \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 10; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $11)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -87,12 +81,17 @@ rm obs exp
 printf "testing --minTumor parameter...\n"
 printf "    loh.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2
-5	112176755	112176756	T	A	APC	0.578313253012	0.567901234568	0.794871794872" > exp
+5	112176755	112176756	T	A	APC	0.578	0.568	0.795" > exp
 oncogemini loh \
     --patient C \
     --minTumor 0.55 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 8; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $9)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -102,35 +101,50 @@ rm obs exp
 printf "testing --specific parameter...\n"
 printf "    loh.patientA...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.A2	alt_AF.A3
-5	112176755	112176756	T	A	APC	0.5	0.983333333333
-6	152419921	152419922	T	A	ESR1	0.407407407407	1.0" > exp
+5	112176755	112176756	T	A	APC	0.500	0.983
+6	152419921	152419922	T	A	ESR1	0.407	1.000" > exp
 oncogemini loh \
     --patient A \
     --specific A3 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    printf("%0.3f\t" "%0.3f\n", $7,$8)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    loh.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B3	alt_AF.B4
-5	56183305	56183306	C	T	MAP3K1	0.542857142857	1.0" > exp
+5	56183305	56183306	C	T	MAP3K1	0.543	1.000" > exp
 oncogemini loh \
     --patient B \
     --specific B4 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    printf("%0.3f\t"  "%0.3f\n", $7,$8)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    loh.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1
-6	152419919	152419920	T	C	ESR1	0.391304347826	1.0" > exp
+6	152419919	152419920	T	C	ESR1	0.391	1.000" > exp
 oncogemini loh \
     --patient C \
     --specific C1 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    printf("%0.3f\t"  "%0.3f\n", $7,$8)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
