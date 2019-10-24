f44e311e5f1c7969fe3f9ff37b36725191364ec2
Thomas Nicholas <u0055382@kingspeak23.wasatch.peaks>
2019-10-24 12:01:45 -0600

    adjusted significant digits in test

--- test/test-bottleneck.sh
+++ test/test-bottleneck.sh
@@ -23,23 +23,33 @@ rm obs exp
 
 printf "    bottleneck.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
-3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287	0.0554716911435	-0.025333518655	0.73779842863
-9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0178571428571	0.35	0.264150943396	0.0878301886792	-0.0492587601078	0.827307416385" > exp
+3	10089729	10089730	C	T	FANCD2	0.000	0.006	0.008	0.268	0.146	0.055	-0.025	0.738
+9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.000	0.000	0.018	0.350	0.264	0.088	-0.049	0.827" > exp
 oncogemini bottleneck \
     --patient B \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 13; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $14)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    bottleneck.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2	slope	intercept	r_value
-6	152419921	152419922	T	A	ESR1	0.0	0.0	0.404255319149	0.202127659574	-0.0673758865248	0.866025403784
-16	68842399	68842400	G	C	CDH1	0.0	0.0	0.588235294118	0.294117647059	-0.0980392156863	0.866025403784" > exp
+6	152419921	152419922	T	A	ESR1	0.000	0.000	0.404	0.202	-0.067	0.866
+16	68842399	68842400	G	C	CDH1	0.000	0.000	0.588	0.294	-0.098	0.866" > exp
 oncogemini bottleneck \
     --patient C \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 11; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $12)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -69,12 +79,17 @@ rm obs exp
 
 printf "    bottleneck.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2	slope	intercept	r_value
-16	68842399	68842400	G	C	CDH1	0.0	0.0	0.588235294118	0.294117647059	-0.0980392156863	0.866025403784" > exp
+16	68842399	68842400	G	C	CDH1	0.000	0.000	0.588	0.294	-0.098	0.866" > exp
 oncogemini bottleneck \
     --patient C \
     --minSlope 0.25 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 11; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $12)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -94,24 +109,34 @@ rm obs exp
 
 printf "    bottleneck.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
-9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0178571428571	0.35	0.264150943396	0.0878301886792	-0.0492587601078	0.827307416385" > exp
+9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.000	0.000	0.018	0.350	0.264	0.088	-0.049	0.827" > exp
 oncogemini bottleneck \
     --patient B \
     --minR 0.8 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 13; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $14)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    bottleneck.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2	slope	intercept	r_value
-6	152419921	152419922	T	A	ESR1	0.0	0.0	0.404255319149	0.202127659574	-0.0673758865248	0.866025403784
-16	68842399	68842400	G	C	CDH1	0.0	0.0	0.588235294118	0.294117647059	-0.0980392156863	0.866025403784" > exp
+6	152419921	152419922	T	A	ESR1	0.000	0.000	0.404	0.202	-0.067	0.866
+16	68842399	68842400	G	C	CDH1	0.000	0.000	0.588	0.294	-0.098	0.866" > exp
 oncogemini bottleneck \
     --patient C \
     --minR 0.8 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 11; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $12)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -131,24 +156,34 @@ rm obs exp
 
 printf "    bottleneck.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
-9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0178571428571	0.35	0.264150943396	0.0878301886792	-0.0492587601078	0.827307416385" > exp
+9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.000	0.000	0.018	0.350	0.264	0.088	-0.049	0.827" > exp
 oncogemini bottleneck \
     --patient B \
     --minEnd 0.25 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 13; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $14)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    bottleneck.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2	slope	intercept	r_value
-6	152419921	152419922	T	A	ESR1	0.0	0.0	0.404255319149	0.202127659574	-0.0673758865248	0.866025403784
-16	68842399	68842400	G	C	CDH1	0.0	0.0	0.588235294118	0.294117647059	-0.0980392156863	0.866025403784" > exp
+6	152419921	152419922	T	A	ESR1	0.000	0.000	0.404	0.202	-0.067	0.866
+16	68842399	68842400	G	C	CDH1	0.000	0.000	0.588	0.294	-0.098	0.866" > exp
 oncogemini bottleneck \
     --patient C \
     --minEnd 0.25 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 11; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $12)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -178,12 +213,17 @@ rm obs exp
 
 printf "    bottleneck.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2	slope	intercept	r_value
-16	68842399	68842400	G	C	CDH1	0.0	0.0	0.588235294118	0.294117647059	-0.0980392156863	0.866025403784" > exp
+16	68842399	68842400	G	C	CDH1	0.000	0.000	0.588	0.294	-0.098	0.866" > exp
 oncogemini bottleneck \
     --patient C \
     --endDiff 0.5 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 11; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $12)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -203,25 +243,35 @@ rm obs exp
 
 printf "    bottleneck.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
-3	10089729	10089730	C	T	FANCD2	0.0	0.00588235294118	0.00806451612903	0.267605633803	0.146496815287	0.0554716911435	-0.025333518655	0.73779842863
-9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0178571428571	0.35	0.264150943396	0.0878301886792	-0.0492587601078	0.827307416385" > exp
+3	10089729	10089730	C	T	FANCD2	0.000	0.006	0.008	0.268	0.146	0.055	-0.025	0.738
+9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.000	0.000	0.018	0.350	0.264	0.088	-0.049	0.827" > exp
 oncogemini bottleneck \
     --patient B \
     --maxNorm 0.6 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 13; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $14)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    bottleneck.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2	slope	intercept	r_value
-5	112176755	112176756	T	A	APC	0.578313253012	0.567901234568	0.794871794872	0.10827927093	0.538749489887	0.844996899579
-6	152419921	152419922	T	A	ESR1	0.0	0.0	0.404255319149	0.202127659574	-0.0673758865248	0.866025403784
-16	68842399	68842400	G	C	CDH1	0.0	0.0	0.588235294118	0.294117647059	-0.0980392156863	0.866025403784" > exp
+5	112176755	112176756	T	A	APC	0.578	0.568	0.795	0.108	0.539	0.845
+6	152419921	152419922	T	A	ESR1	0.000	0.000	0.404	0.202	-0.067	0.866
+16	68842399	68842400	G	C	CDH1	0.000	0.000	0.588	0.294	-0.098	0.866" > exp
 oncogemini bottleneck \
     --patient C \
     --maxNorm 0.6 \
     --columns "chrom,start,end,ref,alt,gene" \
-    oncogemini_test.db > obs
+    oncogemini_test.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 11; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $12)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
