ca1816a253e1334453162836317d9c5ed1982743
Thomas Nicholas <u0055382@kingspeak23.wasatch.peaks>
2019-10-25 12:13:13 -0600

    adjusted significant digits in test

--- test/test-somatic-tools.sh
+++ test/test-somatic-tools.sh
@@ -43,24 +43,34 @@ rm obs exp
 printf "testing --somatic_only parameter...\n"
 printf "    bottleneck.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4	slope	intercept	r_value
-9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.0	0.0	0.0178571428571	0.35	0.264150943396	0.0878301886792	-0.0492587601078	0.827307416385" > exp
+9	102595635	102595654	AACCTTCTCAGCCCTCTCC	A	NR4A3	0.000	0.000	0.018	0.350	0.264	0.088	-0.049	0.827" > exp
 oncogemini bottleneck \
     --patient B \
     --somatic_only \
     --columns "chrom,start,end,ref,alt,gene" \
-    somatic_tests_tools.db > obs
+    somatic_tests_tools.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 13; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $14)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    bottleneck.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2	slope	intercept	r_value
-6	152419921	152419922	T	A	ESR1	0.0	0.0	0.404255319149	0.202127659574	-0.0673758865248	0.866025403784
-16	68842399	68842400	G	C	CDH1	0.0	0.0	0.588235294118	0.294117647059	-0.0980392156863	0.866025403784" > exp
+6	152419921	152419922	T	A	ESR1	0.000	0.000	0.404	0.202	-0.067	0.866
+16	68842399	68842400	G	C	CDH1	0.000	0.000	0.588	0.294	-0.098	0.866" > exp
 oncogemini bottleneck \
     --patient C \
     --somatic_only \
     --columns "chrom,start,end,ref,alt,gene" \
-    somatic_tests_tools.db > obs
+    somatic_tests_tools.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 11; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $12)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
@@ -70,24 +80,34 @@ rm obs exp
 printf "testing --somatic_only parameter...\n"
 printf "    truncal.patientB...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.B0	alt_AF.B1	alt_AF.B2	alt_AF.B3	alt_AF.B4
-6	152419919	152419920	T	C	ESR1	0.0	0.552631578947	0.463414634146	0.383333333333	0.40625" > exp
+6	152419919	152419920	T	C	ESR1	0.000	0.553	0.463	0.383	0.406" > exp
 oncogemini truncal \
     --patient B \
     --somatic_only \
     --columns "chrom,start,end,ref,alt,gene" \
-    somatic_tests_tools.db > obs
+    somatic_tests_tools.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 10; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $11)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
 printf "    truncal.patientC...\n"
 echo "chrom	start	end	ref	alt	gene	alt_AF.C0	alt_AF.C1	alt_AF.C2
-5	56168738	56168740	AC	A	MAP3K1	0.0	0.492753623188	0.476923076923
-5	56183305	56183306	C	T	MAP3K1	0.0	0.415730337079	0.341772151899" > exp
+5	56168738	56168740	AC	A	MAP3K1	0.000	0.493	0.477
+5	56183305	56183306	C	T	MAP3K1	0.000	0.416	0.342" > exp
 oncogemini truncal \
     --patient C \
     --somatic_only \
     --columns "chrom,start,end,ref,alt,gene" \
-    somatic_tests_tools.db > obs
+    somatic_tests_tools.db | \
+    awk '{if ($1=="chrom") print $0}; \
+    {if ($1 != "chrom") \
+    printf("%d\t" "%d\t" "%d\t" "%s\t" "%s\t" "%s\t", $1,$2,$3,$4,$5,$6); \
+    for(i = 7; i <= 8; i++) printf("%0.3f\t"), $i; printf("%0.3f\n", $9)}' | \
+    grep -v "^0.000" > obs
 check obs exp
 rm obs exp
 
